2b1f6782d37b6bc36904d11676480af9b1a69743
sriveraar <phognus@hotmail.com>
2024-12-06 20:26:35 -0300

    Maquetación al 30%

--- tiendapp/urls.py
+++ tiendapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import v_index, v_cart
+from .views import v_index, v_cart, v_product_detail
 
 urlpatterns =[
     path("", v_index, name="index"),
-    path("cart", v_cart, name="cart")
+    path("cart", v_cart, name="cart"),
+    path("product/code", v_product_detail, name="product_detail"),
 ]
--- tiendapp/views.py
+++ tiendapp/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def v_index(request):
     context = {
         "producto": [None, None, None, None, None, None]
     }
     return render(request, "tiendapp/index.html", context)
 
+
 def v_cart(request):
     context = {
         "items": [None, None, None, None]
     }
     return render(request, "tiendapp/cart.html", context)
+
+
+def v_product_detail(request):
+    context = {}
+    return render(request, "tiendapp/product_detail.html", context)
