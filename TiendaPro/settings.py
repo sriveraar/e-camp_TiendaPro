1e969398e64a4d3ae4169ef5dc9bde609b081510
sriveraar <phognus@hotmail.com>
2025-01-02 20:13:21 -0300

    r

--- TiendaPro/settings.py
+++ TiendaPro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#$7z^hp3k$u9gxw)xcixdgh0249!&^=w%w2s54v@j&%p_0tbus
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
